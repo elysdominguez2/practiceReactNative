86b843e85f32d3aeba02d34b99bd00c4ed847e55
elysdominguez <elysdominguez2@gmail.com>
2022-09-20 16:37:59 +0200

    My first App with React Native is created

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from "react";
+import {
+  Text,
+  View,
+  ActivityIndicator,
+  Image,
+  Button,
+  Alert,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const randomNum = useRef(Math.random()).current;
+
+  const [text, onChangeText] = React.useState("");
+
+  const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
+
+  function MyHeader() {
+    return (
+      <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
+          Hello React Native
+        </Text>
+        <ActivityIndicator
+          size="large"
+          color="#c1262c"
+          style={{ marginBottom: 30 }}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+      </View>
+    );
+  }
+
+  function MyFooter() {
+    return (
+      <View>
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: "black",
+            padding: 20,
+            marginBottom: 30,
+          }}
+        >
+          <Text>Hello again!</Text>
+        </View>
+        <Button
+          onPress={() => Alert.alert(text)}
+          title="Alert"
+          color="#c1262c"
+        />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      ListHeaderComponent={MyHeader}
+      data={[0, 1, 2, 3, 4]}
+      renderItem={({ item }) => {
+        return (
+          <TouchableOpacity
+            onPress={() => Alert.alert(`You pressed image #${item + 1}`)}
+          >
+            <Image
+              source={{
+                uri: `https://picsum.photos/500/300?random=${randomNum + item}`,
+              }}
+              style={{ width: "100%", height: 160, marginBottom: 30 }}
+            />
+          </TouchableOpacity>
+        );
+      }}
+      keyExtractor={(item) => String(item)}
+      ListFooterComponent={MyFooter}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
